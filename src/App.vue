54d996aaae92e38ae5aca6729222c3a2a566486c
imamovv <imamovv@gmail.com>
2025-04-27 22:19:32 +0300

    meta test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
 import layout from './components/Layout.vue'
 </script>
 
 <template>
+  <!-- Open Graph Meta Tags -->
+  <meta property="og:title" content="Приглашение на 60-летие" />
+  <meta
+    property="og:description"
+    content="Дорогие друзья! Приглашаем вас на торжественное празднование 60-летия. Будет весело и незабываемо!"
+  />
+  <meta property="og:image" content="https://yourdomain.com/images/celebration.jpg" />
+  <meta property="og:url" content="https://yourdomain.com" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="Приглашение на 60-летие" />
+  <meta property="og:locale" content="ru_RU" />
   <layout></layout>
 </template>
